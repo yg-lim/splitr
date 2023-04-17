32b32497f466a7ea6b8563be087cf3af1077b63a
qseudo <57642143+yg-lim@users.noreply.github.com>
2023-04-17 17:26:53 +0900

    split lists page

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const config = require("./lib/config");
 const app = express();
 const PORT = config.PORT;
 const HOST = config.HOST;
+const PgPersistence = require("./lib/pg-persistence");
+const catchError = require("./lib/catch-error");
 
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  res.locals.store = new PgPersistence();
+  next();
+});
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/", 
+  catchError(async (req, res) => {
+    res.render("split-lists");
+  })
+);
+
+app.use((err, req, res, _next) => {
+  console.log(err.message);
+  res.status(404).send(err.message);
 });
 
 app.listen(PORT, HOST, () => { console.log(`listening on ${PORT}`)} );
